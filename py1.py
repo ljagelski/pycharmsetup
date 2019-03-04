adf98928918d36f0db813981375834efef9a87d8
Len Jagelski <lenstuff@hotmail.com>
2019-03-04 13:24:02 -0800

    updated py1 to handle urls

--- py1.py
+++ py1.py
@@ -1,9 +1,19 @@
 import os
 import argparse
+from utilities import url_utilities
 
 def main(database, url_list_file):
+    big_word_list = []
     print('Db: ' + database)
     print('input list: ' + url_list_file)
+    urls = url_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print('reading: ', url)
+        page_content = url_utilities.load_page(url=url)
+        words = url_utilities.scrape_page(page_contents=page_content)
+        big_word_list.extend(words)
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
